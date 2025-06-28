1705b5a83cdc2908875b22b50c6acda84427cfd2
Aradhana Kuradiya <144244955+Aradhanakuradiya@users.noreply.github.com>
2025-06-28 16:08:32 +0530

    Update AssertionTest.java

--- week2/AssertionsInJUnit/src/test/java/com.ak.junit/AssertionTest.java
+++ week2/AssertionsInJUnit/src/test/java/com.ak.junit/AssertionTest.java
@@ -1,16 +1,16 @@
-package assertions;
+package com.ak.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+public class AssertionsTest {
 
-public class AssertionTest {
- @Test
- public void testAssertions() {
-     assertEquals(5, 2 + 3, "2 + 3 should equal 5");
-     assertTrue(5 > 3, "5 is greater than 3");
-     assertFalse(5 < 3, "5 is not less than 3");
-     assertNull(null, "Object should be null");
-     assertNotNull(new Object(), "Object should not be null");
- }
+    @Test
+    public void testAssertions() {
+        assertEquals(10, 7 + 3, "2 + 3 should equal 10");
+        assertTrue(5 > 3, "5 is greater than 3");
+        assertFalse(5 < 3, "5 is not less than 3");
+        assertNull(null, "Object should be null");
+        assertNotNull(new Object(), "Object should not be null");
+    }
 }
