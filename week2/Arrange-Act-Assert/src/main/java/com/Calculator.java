4fcd8edd0f8c56cc1bff33e141823f0852cc4974
Aradhana Kuradiya <144244955+Aradhanakuradiya@users.noreply.github.com>
2025-06-28 16:56:53 +0530

    Add files via upload

---
+++ week2/Arrange-Act-Assert/src/main/java/com/Calculator.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Calculator {
+	 public int add(int a, int b) {
+		 System.out.println("Addition function called ");
+	        return a + b;
+	    }
+
+	    public int subtract(int a, int b) {
+	    	System.out.println("Subtraction function called ");
+	        return a - b;
+	    }
+}
