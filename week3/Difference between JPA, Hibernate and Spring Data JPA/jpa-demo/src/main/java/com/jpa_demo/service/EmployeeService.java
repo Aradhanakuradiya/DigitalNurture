249a3015fbe20cbe16a27063e46c1d4af6ec5756
Aradhana Kuradiya <144244955+Aradhanakuradiya@users.noreply.github.com>
2025-07-04 20:15:36 +0530

    Add files via upload

---
+++ week3/Difference between JPA, Hibernate and Spring Data JPA/jpa-demo/src/main/java/com/jpa_demo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.jpa_demo.service;
+
+import com.jpa_demo.entity.Employee;
+import com.jpa_demo.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
