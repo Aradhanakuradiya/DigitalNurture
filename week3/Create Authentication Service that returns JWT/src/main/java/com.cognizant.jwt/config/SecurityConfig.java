030ebdb48ebd4d7b751cf540a43604062b566e3f
Aradhana Kuradiya <144244955+Aradhanakuradiya@users.noreply.github.com>
2025-07-13 14:16:54 +0530

    Add files via upload

---
+++ week3/Create Authentication Service that returns JWT/src/main/java/com.cognizant.jwt/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.cognizant.jwt.config;
+
+import com.cognizant.jwt.service.MyUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> {});
+
+        return http.build();
+    }
+}
